51a89476a97f3222db32455bfd7f9735a3f3778f
phuocloc98 <phuocloc17598@gmail.com>
2024-04-01 23:09:16 +0700

    Add library

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import { isbot } from 'isbot';
 
 export function middleware(req: NextRequest) {
   const response = NextResponse.next();
+  const token = req.cookies.get('access_token');
   const { isBot } = userAgent(req);
   console.log(req.nextUrl.pathname);
   console.log('===========================1', isBot);
@@ -18,7 +19,15 @@ export function middleware(req: NextRequest) {
     '444444444444444444444444444444444',
     isbot(req.headers.get('user-agent'))
   );
-
+  if (req.nextUrl.pathname === '/mypage') {
+    const res = NextResponse.next();
+    res.cookies.set('access_token', 'access_tokenaccess_token');
+    return res;
+  }
+  if (!token && req.nextUrl.pathname !== '/mypage') {
+    console.log('================================');
+    return NextResponse.redirect('https://www.google.com/');
+  }
   return response;
 }
 

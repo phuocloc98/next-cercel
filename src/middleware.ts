5a47c3266e55ab5d64d5360b6e1ffe6f528ee37d
phuocloc98 <phuocloc17598@gmail.com>
2024-04-01 21:54:12 +0700

    Add library

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,22 @@
-import { NextRequest, NextResponse, userAgent } from "next/server";
-import { isbot } from "isbot";
+import { NextRequest, NextResponse, userAgent } from 'next/server';
+
+import { isbot } from 'isbot';
 
 export function middleware(req: NextRequest) {
   const response = NextResponse.next();
   const { isBot } = userAgent(req);
   console.log(req.nextUrl.pathname);
-  console.log("===========================1", isBot);
+  console.log('===========================1', isBot);
 
-  console.log("1111111111111111111111111111111111", isBot);
-  console.log("2222222222222222222222222222222222", req.headers.get("isBot"));
+  console.log('1111111111111111111111111111111111', isBot);
+  console.log('2222222222222222222222222222222222', req.headers.get('isBot'));
   console.log(
-    "3333333333333333333333333333333333",
-    req.headers.get("user-agent")
+    '3333333333333333333333333333333333',
+    req.headers.get('user-agent')
   );
   console.log(
-    "444444444444444444444444444444444",
-    isbot(req.headers.get("user-agent"))
+    '444444444444444444444444444444444',
+    isbot(req.headers.get('user-agent'))
   );
 
   return response;
@@ -23,5 +24,5 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher:
-    "/((?!api|_next|static|public|assets|favicon.ico)(?!.*\\.png$)(?!.*\\.jpg$)(?!.*\\.svg$).*)",
+    '/((?!api|_next|static|public|assets|favicon.ico)(?!.*\\.png$)(?!.*\\.jpg$)(?!.*\\.svg$).*)'
 };

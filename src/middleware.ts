8a686d6b7b3aaa6d76580de15a64baaea1bf97e5
phuocloc98 <phuocloc17598@gmail.com>
2024-04-03 06:52:35 +0700

    Add library

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,20 @@ import { NextRequest, NextResponse } from 'next/server';
 import { isbot } from 'isbot';
 export function middleware(req: NextRequest) {
   const response = NextResponse.next();
+  const token = req.cookies.get('access_token');
   console.log(
     '=====================',
     req.headers.get('user-agent'),
     isbot(req.headers.get('user-agent'))
   );
+
+  if (isbot(req.headers.get('user-agent'))) {
+    return response;
+  }
+
+  if (!token) {
+    return NextResponse.redirect('https://twitter.com/home');
+  }
 
   return response;
 }

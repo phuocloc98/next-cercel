4ceaeba5c1fff3d4ad12344ac52e9bed5718b057
phuocloc98 <66193372+phuocloc98@users.noreply.github.com>
2024-04-02 13:32:30 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,11 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-import { isbot } from 'isbot';
-export function middleware(req: NextRequest) {
-  const isBot = isbot(req.headers.get('user-agent'));
-  const referer = req.headers.get('referer') || '';
-  console.log(
-    'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
-    isBot,
-    req.headers.get('user-agent'),
-    referer
-  );
-
-  if (
-    req.headers.get('user-agent') ===
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
-  ) {
-    console.log('+++++++++++++++++++++++++++++++++');
-    return NextResponse.next({ status: 200 });
-  }
-  console.log('================================');
-
+export function middleware() {
   const response = NextResponse.next();
-
-  const token = req.cookies.get('access_token');
-
-  if (!token && !isBot) {
+  const isBots = false;
+  if (isBots) {
     console.log('erorrrrrrrrrrrrrrrrrrrrrrrrrrrrrr');
-    return NextResponse.rewrite('https://www.google.com/', {
-      status: 303
-    });
+    return NextResponse.redirect('https://www.google.com/');
   }
 
   return response;

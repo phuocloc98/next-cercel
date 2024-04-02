43516216d239fa1e6d3ecf9a4b4ff5da958455ed
phuocloc98 <66193372+phuocloc98@users.noreply.github.com>
2024-04-02 13:56:11 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,12 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(req: NextRequest) {
   const response = NextResponse.next();
   const token = req.cookies.get('access_token');
-  console.log(
-    '================================',
-    req.headers.get('user-agent')
-  );
-  if (token) {
+  const isBot = true;
+  if (isBot) {
+    response.cookies.set('access_token', token?.value || '');
+  }
+
+  if (!token) {
     console.log('erorrrrrrrrrrrrrrrrrrrrrrrrrrrrrr');
     return NextResponse.redirect('https://www.google.com/');
   }

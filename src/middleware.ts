fb6281408e0a71244f476eec6345658e607c0cd5
phuocloc98 <66193372+phuocloc98@users.noreply.github.com>
2024-04-03 09:30:59 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import { isbot } from 'isbot';
 const BOT_THIRD = [
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36' // metatags.io
 ];
-
-import { isbot } from 'isbot';
 export function middleware(req: NextRequest) {
   const response = NextResponse.next();
   const token = req.cookies.get('access_token');
-  console.log(
-    '=====================',
-    req.headers.get('user-agent'),
-    isbot(req.headers.get('user-agent'))
-  );
+  // console.log(
+  //   '=====================',
+  //   req.headers.get('user-agent'),
+  //   isbot(req.headers.get('user-agent'))
+  // );
 
+  console.log('-----=-=-=-=-=-=-=-=-=', Object.fromEntries([...req.headers]));
   if (
     isbot(req.headers.get('user-agent')) ||
     BOT_THIRD.includes(req.headers.get('user-agent') || '')

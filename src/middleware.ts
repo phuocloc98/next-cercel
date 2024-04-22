cfdc207a20a9b3e8d6878942ab620212dec9c77e
phuocloc98 <66193372+phuocloc98@users.noreply.github.com>
2024-04-22 21:58:25 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-import { isbot } from 'isbot';
-const BOT_THIRD = [
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36' // metatags.io
-];
 export function middleware(req: NextRequest) {
-  const response = NextResponse.next();
-  const token = req.cookies.get('access_token');
-  const referer = req.headers.get('referer') || '';
-  // console.log(
-  //   '=====================',
-  //   req.headers.get('user-agent'),
-  //   isbot(req.headers.get('user-agent'))
-  // );
+  const headers = new Headers(req.headers);
+  headers.set('x-path-url', req.url);
 
-  console.log('-----=-=-=-=-=-=-=-=-=referer', referer);
-  if (
-    isbot(req.headers.get('user-agent')) ||
-    BOT_THIRD.includes(req.headers.get('user-agent') || '')
-  ) {
-    return response;
-  }
-
-  if (!token) {
-    return NextResponse.redirect('https://twitter.com/home');
-  }
-
-  return response;
+  return NextResponse.next({
+    request: {
+      headers
+    }
+  });
 }
 
 export const config = {
